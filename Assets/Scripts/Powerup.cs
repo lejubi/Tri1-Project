eedb70e9ab126fc2e7e29b3521d637637791ed78
sylviaalee <sylvieelee@gmail.com>
2024-11-28 21:00:01 -0500

    make powerup scriptable object + instances

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Powerup", menuName = "Powerup")]
+public class Powerup : ScriptableObject
+{
+    public float jump;
+    public float speed;
+
+
+}
