641071022a210e6bd5549c866f43d151f0f16790
sylviaalee <sylvieelee@gmail.com>
2024-11-30 23:19:47 -0500

    die when you fall and some more platforms

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,13 +18,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask harmfulObjectLayer;
     [SerializeField] private float invincibilityDuration = 1f;
     private bool isGrounded;
+    private bool playerIsFalling;
+    private float currentFallTime;
+    public float MaxFallTime = 4f;
     private float horizontalMove = 0f;
     private bool isJumping = false;
     public bool hasPowerup;
     private GameManager gameManager;
 
 
-    enum Powerup {
+    enum Powerup
+    {
         Default,
         Jump,
         Speed,
@@ -55,13 +59,14 @@ public class PlayerController : MonoBehaviour
         // checking if player is on the ground and if they are trying to move or jump
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        
+
         if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
             isJumping = true;
         }
 
         UpdateAnimationState();
+        CheckFalling();
     }
 
     void FixedUpdate()
@@ -76,7 +81,7 @@ public class PlayerController : MonoBehaviour
     }
     private void Win()
     {
-        gameManager.winScreen=true;
+        gameManager.winScreen = true;
     }
 
     void UpdateAnimationState()
@@ -119,17 +124,33 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector2(-20, 20);
     }
 
-        // powerup methods
+    private void CheckFalling()
+    {
+        if (!isGrounded)
+        {
+            currentFallTime += Time.deltaTime;
+        }
+        else
+        {
+            currentFallTime = 0;
+        }
+        if (currentFallTime >= MaxFallTime)
+        {
+            gameManager.deathScreen = true;
+        }
+    }
+
+    // powerup methods
     public void TakeDamage(int damage)
     {
-        if(!immunity)
+        if (!immunity)
         {
-            health-=damage;
+            health -= damage;
             health = Mathf.Clamp(health, 0, maxHealth);
             UpdateHealthUI();
             if (health <= 0)
             {
-                gameManager.deathScreen=true;
+                gameManager.deathScreen = true;
             }
         }
     }
@@ -153,25 +174,25 @@ public class PlayerController : MonoBehaviour
         spriteRenderer.enabled = true;
     }
     private IEnumerator FlickerEffect()
-{
-    float flickerInterval = 0.1f; 
-    while (immunity)
     {
-        spriteRenderer.enabled = !spriteRenderer.enabled;
-        yield return new WaitForSeconds(flickerInterval);
+        float flickerInterval = 0.1f;
+        while (immunity)
+        {
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+            yield return new WaitForSeconds(flickerInterval);
+        }
+        spriteRenderer.enabled = true;
     }
-    spriteRenderer.enabled = true; 
-}
     public void addHealth(int h)
     {
-        health+=h;
+        health += h;
         health = Mathf.Clamp(health, 0, maxHealth);
         UpdateHealthUI();
     }
     public void UpdateHealthUI()
     {
         HealthUI healthUI = FindObjectOfType<HealthUI>();
-        if(healthUI!=null)
+        if (healthUI != null)
         {
             healthUI.InitializeHearts();
         }
@@ -198,13 +219,14 @@ public class PlayerController : MonoBehaviour
         jumpForce += force;
     }
 
-    private void PowerupCountdownRoutine() {
+    private void PowerupCountdownRoutine()
+    {
         Invoke(nameof(PowerupEffects), 5);
     }
 
     private void PowerupEffects()
     {
-        switch(powerup)
+        switch (powerup)
         {
             case Powerup.Jump:
                 Debug.Log("Jump powerup");
@@ -218,21 +240,21 @@ public class PlayerController : MonoBehaviour
             case Powerup.Immunity:
                 Debug.Log("Immunity powerup");
                 break;
-            
+
         }
         hasPowerup = false;
     }
 
     // code to check if player has collided with powerup
     // need to fix
-    private void OnCollisionEnter (Collision collisionInfo)
+    private void OnCollisionEnter(Collision collisionInfo)
     {
         Debug.Log("Collided with something");
-        if (collisionInfo.collider.tag == "Powerup");
+        if (collisionInfo.collider.tag == "Powerup") ;
         {
             Debug.Log("Collided with powerup");
             hasPowerup = true;
-            switch(collisionInfo.collider.name)
+            switch (collisionInfo.collider.name)
             {
                 case "Default":
                     powerup = Powerup.Default;
